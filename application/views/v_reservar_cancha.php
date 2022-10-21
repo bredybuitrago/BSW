232d83730e243d68c3c19b1c0948544d1beccdf8
bredybuitrago <bredybuitrago@gmail.com>
2022-10-21 16:56:43 -0500

    cambios reserva canchas

--- application/views/v_reservar_cancha.php
+++ application/views/v_reservar_cancha.php
@@ -9,114 +9,126 @@
 
     <!-- Start Shop Detail  -->
     <div class="shop-detail-box-main">
-        <div class="row">
-          <div class="col-xl-7 col-lg-7 col-md-6">
-            <div id="carousel-example-1" class="single-product-slider carousel slide" data-ride="carousel">
+      <div class="row">
+        <div class="col-xl-6 col-lg-6 col-md-6">
+          <div id="carousel-example-1" class="single-product-slider carousel slide" data-ride="carousel">
 
-              <!-- Carrulsel llenado por javascript  -->
-              <div class="carousel-inner" role="listbox" id="carousel-inner"></div>
+            <!-- Carrulsel llenado por javascript  -->
+            <div class="carousel-inner" role="listbox" id="carousel-inner"></div>
 
-              <a class="carousel-control-prev" href="#carousel-example-1" role="button" data-slide="prev"> 
-                <i class="fa fa-angle-left" aria-hidden="true"></i>
-                <span class="sr-only">Previous</span> 
-              </a>
-              <a class="carousel-control-next" href="#carousel-example-1" role="button" data-slide="next"> 
-                <i class="fa fa-angle-right" aria-hidden="true"></i> 
-                <span class="sr-only">Next</span> 
-              </a>
+            <a class="carousel-control-prev" href="#carousel-example-1" role="button" data-slide="prev"> 
+              <i class="fa fa-angle-left" aria-hidden="true"></i>
+              <span class="sr-only">Previous</span> 
+            </a>
+            <a class="carousel-control-next" href="#carousel-example-1" role="button" data-slide="next"> 
+              <i class="fa fa-angle-right" aria-hidden="true"></i> 
+              <span class="sr-only">Next</span> 
+            </a>
 
-              <!-- Fotos iniatura carrusel llenado por js -->
-              <ol class="carousel-indicators" id="carousel-indicators"></ol>
+            <!-- Fotos iniatura carrusel llenado por js -->
+            <ol class="carousel-indicators" id="carousel-indicators"></ol>
+          </div>
+
+          <div class="container">
+
+            <div class="row my-5">
+              <div class="card card-outline-secondary my-4">
+                <div class="card-header">
+                  <h2>Comentarios de la cancha</h2>
+                </div>
+                <div class="card-body">
+                  <div class="media mb-3">
+                    <div class="mr-2"> 
+                      <img class="rounded-circle border p-1" src="<?php echo base_url(); ?>assets/commerce/images/user_bredi.jpeg" width="64">
+                    </div>
+                    <div class="media-body">
+                      <p>La cancha nos gustó mucho, está limpia y ordenada, la grama esta en buenas condiciones y las empanadas que venden son muy buenas.</p>
+                      <small class="text-muted">Posted by Anonymous on 3/1/22</small>
+                    </div>
+                  </div>
+                  <hr>
+                  <div class="media mb-3">
+                    <div class="mr-2"> 
+                      <img class="rounded-circle border p-1" src="<?php echo base_url(); ?>assets/commerce/images/img-1.jpg" alt="Generic placeholder image" width="64">
+                    </div>
+                    <div class="media-body">
+                      <p>La cancha gustó bastante, muy buenas instalaciones, y el servicio de reserva es muy agil.</p>
+                      <small class="text-muted">Posted by Vinicius Jr. on 3/3/22</small>
+                    </div>
+                  </div>
+                  <hr>
+                  <div class="media mb-3">
+                    <div class="mr-2"> 
+                      <img class="rounded-circle border p-1" src="<?php echo base_url(); ?>assets/commerce/images/img-3.jpg" alt="Generic placeholder image" width="64">
+                    </div>
+                    <div class="media-body">
+                      <p>Good court, good sports environment, excellent web service..</p>
+                      <small class="text-muted">Posted by K Benzema on 3/1/20</small>
+                    </div>
+                  </div>
+                  <hr>
+                  <a href="#" class="btn hvr-hover">Deja tu comentario</a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+        <div class="col-xl-6 col-lg-6 col-md-6">
+          <div class="single-product-details">
+            <h2 id="h2_nombre_local"></h2>
+            <h5 id="h5_barrio"> Monte Blanco</h5>
+            <p class="available-stock"><span> Disponible</span></p>
+            <h4>Descripción</h4>
+            <p id="p_descripcion"></p>
+          </div>
             
-
-
+          <div style="overflow:hidden;border: 1px solid #dee2e6;padding: 20px;">
+            <div class="form-group" style="border: 1px solid #dee2e6;">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div id="datetimepicker13"></div>
+                    </div>
+                </div>
             </div>
           </div>
-          <div class="col-xl-5 col-lg-5 col-md-6">
-            <div class="single-product-details">
-              <h2 id="h2_nombre_local"></h2>
-              <h5 id="h5_barrio"> Monte Blanco</h5>
-              <p class="available-stock"><span> Disponible</span></p>
-              <h4>Descripción</h4>
-              <p id="p_descripcion"></p>
-            </div>
-              
-            <div style="overflow:hidden;border: 1px solid #dee2e6;padding: 20px;">
-              <div class="form-group" style="border: 1px solid #dee2e6;">
-                  <div class="row">
-                      <div class="col-md-12">
-                          <div id="datetimepicker13"></div>
-                      </div>
-                  </div>
-              </div>
-            </div>
 
-            <div class="price-box-bar">
-              <div class="cart-and-bay-btn">
-                <a class="btn hvr-hover" data-fancybox-close="" href="#">Reservar</a>
-                <!-- <a class="btn hvr-hover" data-fancybox-close="" href="#">Más información</a> -->
-              </div>
-            </div>
+          <hr>
+          <h2 class="text-center h2_canchas">CANCHAS</h2>
+          <div id="canchas_horarios" class="canchas_horarios">
+           
+          </div>
 
-              <div class="add-to-btn">
-                <div class="add-comp">
-                  <a class="btn hvr-hover" href="#"><i class="fas fa-heart"></i> Agregar a la lista de "Me gusta"</a>
-                  <a class="btn hvr-hover" href="#"><i class="fas fa-sync-alt"></i> Agregar a comparar</a>
-                </div>
-                <div class="share-bar">
-                  <a class="btn hvr-hover" href="#"><i class="fab fa-facebook" aria-hidden="true"></i></a>
-                  <a class="btn hvr-hover" href="#"><i class="fab fa-google-plus" aria-hidden="true"></i></a>
-                  <a class="btn hvr-hover" href="#"><i class="fab fa-twitter" aria-hidden="true"></i></a>
-                  <a class="btn hvr-hover" href="#"><i class="fab fa-pinterest-p" aria-hidden="true"></i></a>
-                  <a class="btn hvr-hover" href="#"><i class="fab fa-whatsapp" aria-hidden="true"></i></a>
-                </div>
-              </div>
+
+
+         <!--  <div class="price-box-bar">
+            <div class="cart-and-bay-btn">
+              <a class="btn hvr-hover" data-fancybox-close="" href="#">Reservar</a>
             </div>
           </div>
+
+          <div class="add-to-btn">
+            <div class="add-comp">
+              <a class="btn hvr-hover" href="#"><i class="fas fa-heart"></i> Agregar a la lista de "Me gusta"</a>
+              <a class="btn hvr-hover" href="#"><i class="fas fa-sync-alt"></i> Agregar a comparar</a>
+            </div>
+            <div class="share-bar">
+              <a class="btn hvr-hover" href="#"><i class="fab fa-facebook" aria-hidden="true"></i></a>
+              <a class="btn hvr-hover" href="#"><i class="fab fa-google-plus" aria-hidden="true"></i></a>
+              <a class="btn hvr-hover" href="#"><i class="fab fa-twitter" aria-hidden="true"></i></a>
+              <a class="btn hvr-hover" href="#"><i class="fab fa-pinterest-p" aria-hidden="true"></i></a>
+              <a class="btn hvr-hover" href="#"><i class="fab fa-whatsapp" aria-hidden="true"></i></a>
+            </div>
+          </div> -->
         </div>
-        <div class="container">
+      </div>
+    </div>
 
-          <div class="row my-5">
-            <div class="card card-outline-secondary my-4">
-              <div class="card-header">
-                <h2>Comentarios de la cancha</h2>
-              </div>
-              <div class="card-body">
-                <div class="media mb-3">
-                  <div class="mr-2"> 
-                    <img class="rounded-circle border p-1" src="<?php echo base_url(); ?>assets/commerce/images/user_bredi.jpeg" width="64">
-                  </div>
-                  <div class="media-body">
-                    <p>La cancha nos gustó mucho, está limpia y ordenada, la grama esta en buenas condiciones y las empanadas que venden son muy buenas.</p>
-                    <small class="text-muted">Posted by Anonymous on 3/1/22</small>
-                  </div>
-                </div>
-                <hr>
-                <div class="media mb-3">
-                  <div class="mr-2"> 
-                    <img class="rounded-circle border p-1" src="<?php echo base_url(); ?>assets/commerce/images/img-1.jpg" alt="Generic placeholder image" width="64">
-                  </div>
-                  <div class="media-body">
-                    <p>La cancha gustó bastante, muy buenas instalaciones, y el servicio de reserva es muy agil.</p>
-                    <small class="text-muted">Posted by Vinicius Jr. on 3/3/22</small>
-                  </div>
-                </div>
-                <hr>
-                <div class="media mb-3">
-                  <div class="mr-2"> 
-                    <img class="rounded-circle border p-1" src="<?php echo base_url(); ?>assets/commerce/images/img-3.jpg" alt="Generic placeholder image" width="64">
-                  </div>
-                  <div class="media-body">
-                    <p>Good court, good sports environment, excellent web service..</p>
-                    <small class="text-muted">Posted by K Benzema on 3/1/20</small>
-                  </div>
-                </div>
-                <hr>
-                <a href="#" class="btn hvr-hover">Deja tu comentario</a>
-              </div>
-            </div>
-          </div>
-        </div>
+
+
+
+        
+
         <div>
           <div class="row my-5">
             <div class="col-lg-12">
@@ -284,44 +296,7 @@
         </div>
       </div>
 
-      <table id="example" class="tabla table-bordered" style="    width: 30%;
-    text-align: center;
-    margin-left: 341px;">
-        <tbody>
-
-          <tr>
-            <td>am</td>
-            <td>12</td>
-            <td>1</td>
-            <td>2</td>
-            <td>3</td>
-            <td>4</td>
-            <td>5</td>
-            <td>6</td>
-            <td>7</td>
-            <td>8</td>
-            <td>9</td>
-            <td>10</td>
-            <td>11</td>
-          </tr>
-          <tr>
-            <td>pm</td>
-            <td>12</td>
-            <td>1</td>
-            <td>2</td>
-            <td>3</td>
-            <td>4</td>
-            <td>5</td>
-            <td>6</td>
-            <td>7</td>
-            <td>8</td>
-            <td>9</td>
-            <td>10</td>
-            <td>11</td>
-          </tr>
-
-        </tbody>
-      </table>
+      
 
 
     </div>
--- assets/js/reservar_cancha.js
+++ assets/js/reservar_cancha.js
@@ -13,6 +13,7 @@ $(function () {
 		//Eventos de la ventana.
 		events: function () {
 			$('#datetimepicker13').on("change.datetimepicker", reservar.listarCanchasHorarios);
+			$('#canchas_horarios').on('click','td.td_hora', reservar.hora_click);
 		},
 
 		getDataLocal: function() {
@@ -27,7 +28,6 @@ $(function () {
 				reservar.mostrarCarrusel(local.fotos);
 				reservar.llenarIndicadoresFotos(local.fotos);
 				reservar.llenarDatosLocal(local);
-
 			});
 		},
 		
@@ -44,11 +44,8 @@ $(function () {
 
 		llenarIndicadoresFotos: function (fotos){
 			let active = '';
-
 			fotos.forEach(function(foto, indice_foto) {
 				active = (indice_foto == 0)? 'class="active"' : "";
-
-
 				$('#carousel-indicators').append(
 					`
 						<li data-target="#carousel-example-1" data-slide-to="${indice_foto}" ${active}><img class="d-block w-100 img-fluid" src="${base_url}${foto.ruta}" alt="Foto Local" /></li>
@@ -58,8 +55,8 @@ $(function () {
 
 		llenarDatosLocal: function(local){
 			$('#p_descripcion').text(local.descripcion);
-			$('#h2_nombre_local').text(local.nombre_local);
-			$('#h5_barrio').text(local.barrio);
+			$('#h2_nombre_local').text("LOCAL: " + local.nombre_local);
+			$('#h5_barrio').text("BARRIO: " + local.barrio);
 
 
 			let diasDeshabilitados = [0,1,2,3,4,5,6];
@@ -96,13 +93,12 @@ $(function () {
 			reservar.diasDeshabilitados = diasDeshabilitados.filter((diaDeshabilitado, indice) => diaDeshabilitado != -1);
 
 			calendario = $('#datetimepicker13').datetimepicker({
-          inline: true,
-          format: 'L',
-          daysOfWeekDisabled: reservar.diasDeshabilitados,
-          locale: moment.locale('es'),
-          minDate: new Date()
-      });
-
+				inline: true,
+				format: 'L',
+				daysOfWeekDisabled: reservar.diasDeshabilitados,
+				locale: moment.locale('es'),
+				minDate: new Date()
+			});
 		},
 
 		listarCanchasHorarios: function(){
@@ -117,10 +113,138 @@ $(function () {
 			},
 			// función que recibe los datos
 			function(data) {
-				const obj = JSON.parse(data);
-				console.log(obj)
-				
+				const canchas = JSON.parse(data);
+				console.log(canchas)
+				reservar.crearHorariosCanchas(canchas);
 			});
+		},
+
+		crearHorariosCanchas: function(canchas){
+			$('#canchas_horarios').html('');
+			canchas.forEach(function(cancha, indice_cancha){
+				$('#canchas_horarios').append(
+					`
+					<fieldset class="border p-2 single-product-details fs_canchas">
+		                <legend  class="float-none w-auto p-2">${cancha.identificacion}</legend>
+		                <h5>${cancha.tipo_cancha}</h5>              
+		                <p>${cancha.observacion}</p>
+		                <table class="table table-bordered text-center tabla_standar table-sm">
+		                  <tbody>
+		                    <tr>
+		                      <td class="td_hora" data-hour="12" data-franja="am" data-cancha_id="${cancha.cancha_id}" data-cancha_identificacion="${cancha.identificacion}">12</td>
+		                      <td class="td_hora" data-hour="1" data-franja="am" data-cancha_id="${cancha.cancha_id}" data-cancha_identificacion="${cancha.identificacion}">1</td>
+		                      <td class="td_hora" data-hour="2" data-franja="am" data-cancha_id="${cancha.cancha_id}" data-cancha_identificacion="${cancha.identificacion}">2</td>
+		                      <td class="td_hora" data-hour="3" data-franja="am" data-cancha_id="${cancha.cancha_id}" data-cancha_identificacion="${cancha.identificacion}">3</td>
+		                      <td class="td_hora" data-hour="4" data-franja="am" data-cancha_id="${cancha.cancha_id}" data-cancha_identificacion="${cancha.identificacion}">4</td>
+		                      <td class="td_hora" data-hour="5" data-franja="am" data-cancha_id="${cancha.cancha_id}" data-cancha_identificacion="${cancha.identificacion}">5</td>
+		                      <td class="td_hora" data-hour="6" data-franja="am" data-cancha_id="${cancha.cancha_id}" data-cancha_identificacion="${cancha.identificacion}">6</td>
+		                      <td class="td_hora" data-hour="7" data-franja="am" data-cancha_id="${cancha.cancha_id}" data-cancha_identificacion="${cancha.identificacion}">7</td>
+		                      <td class="td_hora" data-hour="8" data-franja="am" data-cancha_id="${cancha.cancha_id}" data-cancha_identificacion="${cancha.identificacion}">8</td>
+		                      <td class="td_hora" data-hour="9" data-franja="am" data-cancha_id="${cancha.cancha_id}" data-cancha_identificacion="${cancha.identificacion}">9</td>
+		                      <td class="td_hora" data-hour="10" data-franja="am" data-cancha_id="${cancha.cancha_id}" data-cancha_identificacion="${cancha.identificacion}">10</td>
+		                      <td class="td_hora" data-hour="11" data-franja="am" data-cancha_id="${cancha.cancha_id}" data-cancha_identificacion="${cancha.identificacion}">11</td>
+		                      <td class="franja_am">am <i class="far fa-sun"></i></td>
+		                    </tr>
+		                    <tr>
+		                      <td class="td_hora" data-hour="12" data-franja="pm" data-cancha_id="${cancha.cancha_id}" data-cancha_identificacion="${cancha.identificacion}">12</td>
+		                      <td class="td_hora" data-hour="1" data-franja="pm" data-cancha_id="${cancha.cancha_id}" data-cancha_identificacion="${cancha.identificacion}">1</td>
+		                      <td class="td_hora" data-hour="2" data-franja="pm" data-cancha_id="${cancha.cancha_id}" data-cancha_identificacion="${cancha.identificacion}">2</td>
+		                      <td class="td_hora" data-hour="3" data-franja="pm" data-cancha_id="${cancha.cancha_id}" data-cancha_identificacion="${cancha.identificacion}">3</td>
+		                      <td class="td_hora" data-hour="4" data-franja="pm" data-cancha_id="${cancha.cancha_id}" data-cancha_identificacion="${cancha.identificacion}">4</td>
+		                      <td class="td_hora" data-hour="5" data-franja="pm" data-cancha_id="${cancha.cancha_id}" data-cancha_identificacion="${cancha.identificacion}">5</td>
+		                      <td class="td_hora" data-hour="6" data-franja="pm" data-cancha_id="${cancha.cancha_id}" data-cancha_identificacion="${cancha.identificacion}">6</td>
+		                      <td class="td_hora" data-hour="7" data-franja="pm" data-cancha_id="${cancha.cancha_id}" data-cancha_identificacion="${cancha.identificacion}">7</td>
+		                      <td class="td_hora" data-hour="8" data-franja="pm" data-cancha_id="${cancha.cancha_id}" data-cancha_identificacion="${cancha.identificacion}">8</td>
+		                      <td class="td_hora" data-hour="9" data-franja="pm" data-cancha_id="${cancha.cancha_id}" data-cancha_identificacion="${cancha.identificacion}">9</td>
+		                      <td class="td_hora" data-hour="10" data-franja="pm" data-cancha_id="${cancha.cancha_id}" data-cancha_identificacion="${cancha.identificacion}">10</td>
+		                      <td class="td_hora" data-hour="11" data-franja="pm" data-cancha_id="${cancha.cancha_id}" data-cancha_identificacion="${cancha.identificacion}">11</td>
+		                      <td class="franja_pm">pm <i class="far fa-moon"></i></td>
+		                    </tr>
+
+		                  </tbody>
+		                </table>
+		            </fieldset>
+					`
+				);
+
+
+			});
+		},
+
+		hora_click: function(){
+			const hora = $(this).data('hour');
+			const franja = $(this).data('franja');
+			const cancha_id = $(this).data('cancha_id');
+			const cancha_identificacion = $(this).data('cancha_identificacion');
+			const nombre_local = $('#h2_nombre_local').text();
+
+			let maximo = '4';
+
+			console.log(hora)
+			console.log(franja)
+			if (hora == '9' && franja == 'pm') {
+				maximo = '3';
+			} else if (hora == '10' && franja == 'pm') {
+				maximo = '2';
+			} else if (hora == '11' && franja == 'pm') {
+				maximo = '1';
+			}
+
+			Swal.fire({
+		        html: `
+		        	<h2 class="swal2-title" id="swal2-title" style="display: block;">¿Cuántas horas quieres reservar?</h2>
+		        	<label for="swal2-input" class="swal2-input-label">ingresa máximo ${maximo}</label>
+		        	<input type="number" value="1" class="swal2-input" id="range-value" max="${maximo}" min="1" style="text-align: center;">
+		        `,
+		        icon: 'question',
+		        confirmButtonText: 'Confirmar',
+		        showCancelButton: true,
+		       	cancelButtonText: 'Cancelar',
+		       	confirmButtonColor: '#1f844b',
+  				//cancelButtonColor: '#d33',
+  				width: '800px',
+		        preConfirm: function() {
+		            return new Promise((resolve, reject) => {
+		                resolve(
+		                    $('input[type="number"]').val()
+		                );
+		            });
+		        }
+		    }).then((data) => {
+		        console.log(data);
+		    	if (data.isConfirmed) {
+		    		const date = $('#datetimepicker13').datetimepicker('viewDate');
+		    		const fecha_calendario = moment(date._d).format("YYYY-MM-DD");
+		    		Swal.fire({
+					  	html: `
+			        		<h2 class="swal2-title" id="swal2-title" style="display: block;">¡Confirma estos datos!</h2>
+			        		<label for="swal2-input" class="swal2-input-label">fecha : ${fecha_calendario}</label>
+			        		<label for="swal2-input" class="swal2-input-label">Hora inicio: ${hora}:00${franja}</label>
+			        		<label for="swal2-input" class="swal2-input-label">Cantidad horas: ${data.value}</label>
+			        		<label for="swal2-input" class="swal2-input-label">${nombre_local} | cancha: ${cancha_identificacion}</label>
+			        	`,
+					  	icon: 'warning',
+					  	showCancelButton: true,
+					  	confirmButtonColor: '#1f844b',
+					  	cancelButtonColor: '#d33',
+					  	confirmButtonText: 'Sí, Separar cancha!',
+					  	cancelButtonText: 'Cancelar',
+					}).then((result) => {
+					  	if (result.isConfirmed) {
+					    	Swal.fire(
+						     	'Deleted!',
+						      	'Your file has been deleted.',
+						      	'success'
+						    )
+					  	} else {
+					  		helper.miniAlert();
+					  	}
+					})
+		    	}
+
+
+		    });
+
 
 
 
